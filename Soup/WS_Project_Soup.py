edab9a2cae7487233f91bade34b50f9c6e7ecc6a
duae-marriam <128074937+duae-marriam@users.noreply.github.com>
2023-06-08 15:53:40 +0200

    Create WS_Project_Soup.py

---
+++ Soup/WS_Project_Soup.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from urllib import request
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+url = ['https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=1','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=2','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=3','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=4','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=5','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=6']
+
+phone_links = []
+phone_specs =[]
+for i in url:
+    try:
+        response = requests.get(i)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the phone details on the page
+        phone_details = soup.find_all('div', {'class': '_1AtVbE'})
+
+        for phone in phone_details[3:]:
+
+            herf = str('https://www.flipkart.com'+phone.find('a')['href'])
+            phone_links.append(herf)
+            
+
+
+
+            #Extract relevant information
+            name = phone.find('div', {'class': '_4rR01T'}).text.split(">")
+            price = phone.find('div', {'class': '_30jeq3 _1_WHN1'}).text.split(">")
+            rating = phone.find('div', {'class': '_3LWZlK'}).text.split(">")
+            Memory = phone.find('li', {'class': 'rgWa7D'}).text
+            Screen = phone.find('li', {'class': 'rgWa7D'}).find_next_sibling().text
+            Camera = phone.find('li', {'class': 'rgWa7D'}).find_next_sibling().find_next_sibling().text
+            Battery = phone.find('li', {'class': 'rgWa7D'}).find_next_sibling().find_next_sibling().find_next_sibling().text
+            Processor = phone.find('li', {'class': 'rgWa7D'}).find_next_sibling().find_next_sibling().find_next_sibling().find_next_sibling().text
+            Warranty = phone.find('li', {'class': 'rgWa7D'}).find_next_sibling().find_next_sibling().find_next_sibling().find_next_sibling().find_next_sibling().text
+
+            #I can adjust the below line of code to have all the separate elements of the phones as a full data frame too
+            
+            phone_specs.append(', '.join([item[0] for item in [name, price, rating]])+ Memory+Screen+ Camera+ Battery+Warranty+Processor)
+    except:
+        print('error here')
+
+
+#Export the Phone_links as a text file
+with open('Phone_links.txt', 'w') as outfile:
+    outfile.writelines((str(i)+'\n' for i in phone_links))
+#We can either be converting into a Dataframe and them exporting
+df = pd.DataFrame(phone_specs)
+
+# Export the DataFrame phone_specs as a text file
+df.to_csv('Phone specs.txt', sep='\t', index=False)
+
